08c8926b84f393aedefd214d8d7650ff54c60f3c
AlexMitushkin <am2010zp@gmail.com>
2023-03-14 13:17:15 +0200

    js popUp
    js popUp

--- main.js
+++ main.js
@@ -1,26 +1,14 @@
-const openPopUP = document.getElementsByClassName('popup__open')
-// const openPopUP1 = document.getElementById('popup__open1')
-// const openPopUP2 = document.getElementById('popup__open2')
-const closePopUP = document.getElementById('popup__close')
+const popUp = document.getElementById("popup");
 
-const popUp = document.getElementById('popup')
 
-openPopUP.addEventListener('click', function(e){
-   e.preventDefault();
-   popUp.classList.add('active');
+document.getElementById("popup__close").addEventListener("click", () => {
+  popUp.classList.remove("active");
+});
+
+document.querySelectorAll(".popup__open").forEach((elem) => {
+  elem.addEventListener("click", () => {
+    popUp.classList.add("active");
+  });
 })
 
-// openPopUP1.addEventListener('click', function(e){
-//    e.preventDefault();
-//    popUp.classList.add('active');
-// }
-// )
-// openPopUP2.addEventListener('click', function(e){
-//    e.preventDefault();
-//    popUp.classList.add('active');
-// })
-
-closePopUP.addEventListener('click' ,() => {
-   popUp.classList.remove('active');
-})
 
